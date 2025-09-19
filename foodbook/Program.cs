ad7991411a6e604b28b3764e9a998a049ec22c1f
Amtia <huhume147@gmail.com>
2025-09-19 22:15:07 +0700

    login UI

--- foodbook/Program.cs
+++ foodbook/Program.cs
@@ -28,7 +28,7 @@ namespace foodbook
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Login}/{id?}");
 
             app.Run();
         }
